b52c012577cce8ad4a82d27057d82a507af87492
ibrahim <ibrahim.mohamed@movired.com>
2026-03-26 13:26:55 +0100

    configuracion uso movil y web

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,5 +1,5 @@
 // Detectamos si estamos en la App de Capacitor (móvil) o en el navegador del PC
-const isCapacitor = window.location.protocol === 'capacitor:';
+const isCapacitor = window.location.protocol === 'capacitor:' || !!window.Capacitor?.isNativePlatform();
 
 // Tu IP de red local (para el móvil)
 const PC_IP = "192.168.18.156";
